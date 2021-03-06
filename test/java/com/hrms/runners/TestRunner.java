1780a3510edc4be68cf5ea98967dc139c8faf5f8
emineakbulut <ezz.akbulut@gmail.com>
2021-03-06 10:28:04 -0800

    no change

--- test/java/com/hrms/runners/TestRunner.java
+++ test/java/com/hrms/runners/TestRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
                            "json:target/cucumber.json"// generates json report
                           }
          )
-public class TestRunner {
+public class TestRunner {//oidhfsih
 
 
 }
